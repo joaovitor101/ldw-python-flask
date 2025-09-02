5ea1565fa88a1cadb7abfe3e501e82e5a9041969
DSM4 <fatec-dsm4@academico.intra>
2025-09-01 22:18:34 -0300

    versão 0.0.6 aula 04

---
+++ ldw-aula-04-crud-com-sqlite/app.py
@@ -0,0 +1,25 @@
+from flask import Flask, render_template #importa a biblioteca flask
+from controllers import routes
+#criando uma instancia da classe Flask
+from models.database import db
+#importanto biblioca para manipulação do S.O
+import os
+
+app = Flask(__name__, template_folder='views', static_folder='static')
+routes.init_app(app) 
+dir = os.path.abspath(os.path.dirname(__file__))
+#configurando o banco de dados
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + dir + os.path.join('models/games.sqlite3')
+db.init_app(app)
+with app.app_context():
+    db.create_all()
+    
+if __name__ == '__main__':
+    #verificar no início da aplicação se o banco de dados existe, senão criar
+    with app.test_request_context():
+        db.create_all()
+    
+    app.run(debug = True, port = 5000, host = '0.0.0.0')
+
+
+
